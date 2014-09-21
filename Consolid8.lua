ec8eee59ceb8b19d903ab933c9f4109368adb28e
Harry Cutts <eternal.linux@gmail.com>
2014-09-21 18:22:58 +0100

    Release 1.3, from WoWInterface archives
    Change log:
    * Now consolidates messages from looting, gathering, etc.
    * Now tracks XP gains.
    * Changed the report method to a static pop-up, and made it appear
      automatically when you leave an instance.
    * Added slash command switches /consolid8 show and /consolid8 hide
      which show and hide the button respectively.

--- Consolid8.lua
+++ Consolid8.lua
@@ -1,4 +1,4 @@
---[[	
+--[[
 	Consolid8, a World of Warcraft chat frame addon
 	Copyright 2010 Harry Cutts
 
@@ -17,29 +17,34 @@
     You should have received a copy of the GNU General Public License
     along with Consolid8.  If not, see <http://www.gnu.org/licenses/>.
 ]]--
+--[[Consolid8_Settings MEMBERS
+	loot  : True if loot consolidation is enabled; else false.
+	scale : Stores the scale of the button.
+]]
 
 local addOnName, L = ...	-- Local locale table
 
+-- [[ Data ]]--
+local originalHonor
+local originalXP
 local data = {}
 local specialData = {
 	-- Money : money looted
 }
-Consolid8 = {
-	data = data,
-	specialData = specialData,
-}
+Consolid8 = { data = data, specialData = specialData, }
 
---[[ Local Functions ]]--
+--[[ Utility Functions ]]--
 
-local function print(message)
-	-- Writes a message to the chat frame, prefixed by "Consolid8: " in color.
-	-- message: The string to print in the chat frame
-	DEFAULT_CHAT_FRAME:AddMessage("|cFF0080FF" .. addOnName .. ":|r " .. message)
+local function print(msg)
+	DEFAULT_CHAT_FRAME:AddMessage("|cFF0080FF" .. addOnName .. ":|r " .. tostring(msg))
+end
+
+local lootMsgInfo = ChatTypeInfo["LOOT"]
+local function LootPrint(msg)
+	DEFAULT_CHAT_FRAME:AddMessage(msg, lootMsgInfo.r, lootMsgInfo.g, lootMsgInfo.b)
 end
 
 local function CoppersToString(copper)
-	-- Formats a money string from a given number of coppers
-	--  copper: the number of coppers
 	-- Returns: a formatted money string, e.g. "1g 56s 20c" when copper = 15620
 	local gold 		= math.floor(copper / 10000)
 	copper 			= copper % 10000
@@ -50,38 +55,72 @@ end
 
 local function StringToCoppers(str)
 	-- Returns: the amount of money in str, in coppers.
-	--     str: the string to match.
 	local gold   = str:match(L["MONEY_GOLD"]) 	or 0
 	local silver = str:match(L["MONEY_SILVER"]) or 0
 	local copper = str:match(L["MONEY_COPPER"]) or 0
 	return (gold * 10000) + (silver * 100) + copper
 end
 
-local function ChangeData(faction, change)
-	-- Changes the data in the data table to reflect the change of reputation.
-	-- faction: The faction with whom reputation has changed.
-	--  change: The amount by which reputation has changed. Negative values decrement the reputation.
-	data[faction] = (data[faction] or 0) + change
-end
-
-local function ChangeSpecialData(key, change)
-	-- Changes the data in the data table to reflect the change of reputation.
-	--     key: The data to change.
-	--  change: The amount by which the data has changed. Negative values decrement the data.
-	specialData[key] = (specialData[key] or 0) + change
-end
-
 --[[ Public Functions ]]--
 
+StaticPopupDialogs.Consolid8_Report = {
+	text         	= "|cFF0080FF" .. addOnName .. ":|r " .. L["REPORT"] .. ":\n",
+	button1      	= RESET,	-- Accept button
+	button2			= OKAY,		-- Cancel button
+	
+	OnShow			= function(self)
+		local namesStr  = ""
+		local valuesStr = ""
+		
+		for key, value in pairs(data) do
+			namesStr  = namesStr  .. key .. "\n"
+			valuesStr = valuesStr .. value .. "\n"
+		end
+		
+		-- Money, Honor, and XP
+		if specialData.money then
+			namesStr  = namesStr  .. "\n|TInterface\\Icons\\INV_Misc_Coin_01:16|t " .. MONEY
+			valuesStr = valuesStr .. "\n" .. CoppersToString(specialData.money)
+		end
+		
+		local honorGain = GetHonorCurrency() - originalHonor
+		if honorGain ~= 0 then
+			namesStr  = namesStr  .. "\n" .. HONOR
+			valuesStr = valuesStr .. "\n" .. honorGain
+		end
+		
+		local namesFS = self:CreateFontString(nil, "ARTWORK", "Consolid8NameStyle")
+		self.namesFS = namesFS;
+		namesFS:SetPoint("LEFT", self, "LEFT", 15, 0)
+		namesFS:SetText(namesStr)
+		
+		local valuesFS = self:CreateFontString(nil, "ARTWORK", "Consolid8ValueStyle")
+		self.valuesFS = valuesFS;
+		valuesFS:SetPoint("RIGHT", self, "RIGHT", -15, 0)
+		valuesFS:SetText(valuesStr)
+		
+		self.height = self:GetHeight() + namesFS:GetStringHeight()
+	end,
+	
+	OnUpdate		= function(self)
+		self:SetHeight(self.height)
+	end,
+	
+	OnAccept		= function()
+		Consolid8.Reset()
+	end,
+	
+	OnHide			= function(self)
+		self.namesFS:Hide()
+		self.valuesFS:Hide()
+	end,
+	
+	whileDead = 1, hideOnEscape = 1, notClosableByLogout = 1, timeout = 0,
+};
+
 function Consolid8.Report()
-	-- Dumps the contents of the data table to the default chat frame.
-	print(L["REPORT"] .. ":")
-	for key, value in pairs(data) do
-		print(format("%s = %d", key, value))
-	end
-	for key, value in pairs(specialData) do
-		print(format("%s = %d", key, value))
-	end
+	-- Displays the report dialog.
+	local popup = StaticPopup_Show("Consolid8_Report")
 end
 
 function Consolid8.Reset()
@@ -92,54 +131,172 @@ function Consolid8.Reset()
 	for key, value in pairs(specialData) do
 		specialData[key] = nil
 	end
+	originalHonor	= GetHonorCurrency()
+	originalXP		= UnitXP("player")
 	print(RESET)
 end
 
---[[ AddOn Methods ]]--
+--[[ Event Handling ]]--
 
 local frame
-local originalHonor
+
+-- Loot message consolidation
+local looting 	  = false					-- True if the loot frame is open; else false.
+local lootString
+
+local inInstance = false					-- Instance tracking. Initialized on frame load.
+
+local function ChangeData(faction, change)
+	-- Changes the data in the data table to reflect the change of reputation.
+	data[faction] = (data[faction] or 0) + change
+end
+
+local function ChangeSpecialData(key, change)
+	-- Changes the data in the data table to reflect the change of reputation.
+	specialData[key] = (specialData[key] or 0) + change
+end
+
+local function LogLoot(msg)
+	local str = msg:match(L["LOOT"])
+	if str then
+		lootString = ( lootString and (lootString .. ", " .. str) ) or str
+	else
+		LootPrint(msg)
+	end
+end
+
+local function StopLogging()
+	looting = false
+	if lootString then
+		LootPrint(format(LOOT_ITEM_SELF, lootString))
+		lootString = nil
+	end
+end
+
+local eventHandlers
+eventHandlers = {
+	CHAT_MSG_COMBAT_FACTION_CHANGE = function(msg)	-- Reputation
+		-- Attempt to match the increased pattern string
+		local faction, change = msg:match(L["REP_INC"])
+		
+		if change then
+			ChangeData(faction, change)
+		else
+			-- Attempt to match the decreased pattern string
+			faction, change = msg:match(L["REP_DEC"])
+			
+			if change --[[a match has been found]] then ChangeData(faction, -change) end
+		end
+	end,
+	
+	CHAT_MSG_MONEY = function(msg)					-- Money
+		ChangeSpecialData("money", StringToCoppers(msg))
+		
+		if Consolid8_Settings.loot and type(looting) == "number" then -- Money loot is counted as an item for GetNumLootItems()
+			looting = looting - 1
+			if looting == 0 then
+				StopLogging()
+			end
+		end
+	end,
+	
+	--[[ Looting ]]--
+	LOOT_OPENED = function(autolooting)				-- Loot: start logging
+		-- autolooting: 1 if autolooting, else 0 (NOT NIL!)
+		if not Consolid8_Settings.loot then return end
+		if looting then
+			StopLogging()
+			return
+		end
+		if arg1 == 1 then
+			looting = GetNumLootItems()
+		else 
+			looting = true
+		end
+	end,
+	
+	CHAT_MSG_LOOT = function(msg)						  -- log message
+		if not Consolid8_Settings.loot then return end
+		
+		if not looting then	-- Pass the message on as normal
+			LootPrint(msg)
+		else
+			LogLoot(msg)
+			
+			if type(looting) == "number" then	-- See whether this was the last item
+				looting = looting - 1
+				if looting == 0 then
+					StopLogging()
+				end
+				
+			elseif not LootFrame:IsShown() then	-- See if the loot frame has been closed
+				StopLogging()
+			end
+		end
+	end,
+
+	UI_ERROR_MESSAGE = function(msg)					-- check for full inventory etc.
+		if type(looting) == "number" and (msg == ERR_INV_FULL or msg == ERR_LOOT_CANT_LOOT_THAT
+				or msg == ERR_LOOT_CANT_LOOT_THAT_NOW or msg == ERR_LOOT_ROLL_PENDING) then
+			looting = looting - 1
+			if looting == 0 then
+				StopLogging()
+			end
+		end
+	end,
+	
+	--[[  ]]--
+	PLAYER_ENTERING_WORLD = function()				-- Check for leaving an instance
+		if IsInInstance() then
+			inInstance = true
+		else
+			if inInstance and not UnitIsDeadOrGhost("player") then
+				Consolid8.Report()
+				inInstance = false
+			end
+		end
+	end,
+	
+	ADDON_LOADED = function(name)					-- Load saved variables (self-destructs)
+		if name ~= addOnName then return end
+		
+		if not Consolid8_Settings then
+			Consolid8_Settings = { loot = true, visible = true }
+		end
+		if not Consolid8_Settings.visible then
+			frame:Hide()
+		end
+		
+		-- Self-destruct
+		frame:UnregisterEvent("ADDON_LOADED")
+		eventHandlers.ADDON_LOADED = nil
+	end,
+	
+	PLAYER_LOGIN = function()						-- Initialization stuff
+		-- Set the scale to be the same as the other chat buttons
+		frame:SetScale(Consolid8_Settings.scale or ChatFrameMenuButton:GetScale())
+		
+		-- Record the starting honor and XP
+		originalHonor 	= GetHonorCurrency()
+		originalXP		= UnitXP("player")
+	end,
+}
 
 function Consolid8.OnLoad()
 	-- Initialize local and public variables
-	frame = Consolid8_Frame;
-	Consolid8.frame = frame;
+	frame = Consolid8_Frame
+	Consolid8.frame = frame
+	inInstance = IsInInstance()
 	
 	-- Register events
-	frame:RegisterEvent("PLAYER_LOGIN")
-	frame:RegisterEvent("CHAT_MSG_COMBAT_FACTION_CHANGE")
-	frame:RegisterEvent("CHAT_MSG_MONEY")
+	for event in pairs(eventHandlers) do
+		frame:RegisterEvent(event)
+	end
 end
 
-function Consolid8.OnEvent(self, event, arg1, ...)
-	if event == "CHAT_MSG_COMBAT_FACTION_CHANGE" then
-		-- arg1: the message to be printed to the chat frame.
-		-- Attempt to match the increased pattern string
-		local faction, change = arg1:match(L["REP_INC"])
-		
-		if change --[[a match has been found]] then
-			ChangeData(faction, change)
-			
-		else
-			-- Attempt to match the decreased pattern string
-			faction, change = arg1:match(L["REP_DEC"])
-			
-			if change --[[a match has been found]] then
-				ChangeData(faction, -change)
-			end
-		end
-		
-	elseif event == "CHAT_MSG_MONEY" then
-		ChangeSpecialData("Money", StringToCoppers(arg1))
-	
-	elseif event == "PLAYER_LOGIN" then
-		-- arg1: the name of the addOn which has been loaded.
-		-- Set the scale to be the same as the other chat buttons
-		frame:SetScale(Consolid8_Scale or ChatFrameMenuButton:GetScale())
-		
-		-- Record the starting honor
-		originalHonor = GetHonorCurrency()
-	end -- if event
+function Consolid8.OnEvent(event, ...)
+	local handler = eventHandlers[event]
+	if handler then	handler(...) end
 end
 
 --[[ Slash handler ]]--
@@ -151,29 +308,25 @@ SlashCmdList["CONSOLID"] = function(msg)
 		Consolid8.Report()
 	elseif msg == string.lower(RESET) then
 		Consolid8.Reset()
+	elseif msg == string.lower(L["SHOW"]) then
+		Consolid8.Show()
+	elseif msg == string.lower(L["HIDE"]) then
+		Consolid8.Hide()
+	else
+		print(SLASH_CONSOLID1 .. format(" %s, %s, %s, %s", L["REPORT"], RESET, L["SHOW"], L["HIDE"]))
 	end
 end
 
---[[ Broker plugin ]]--
+--[[ Show/hide button ]]--
 
-local watch 	= nil
-local dataObj 	= LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Consolid8",
-	{
-		type 	= "data source",
-		icon 	= "Interface\\AddOns\\Consolid8\\Broker",
-		label 	= addOnName,
-	})
-	
-function dataObj.OnClick(self --[[, button]])
-	Consolid8.ShowMenu(self)
+function Consolid8.Show()
+	frame:Show()
+	Consolid8_Settings.visible = true
 end
 
-function dataObj.OnEnter(self)
-	Consolid8.ShowTooltip()
-end
-
-function dataObj.OnLeave(self)
-	Consolid8.HideTooltip()
+function Consolid8.Hide()
+	frame:Hide()
+	Consolid8_Settings.visible = false
 end
 
 --[[ Tooltip ]]--
@@ -198,19 +351,24 @@ function Consolid8.ShowTooltip()
 	tooltip:AddDoubleLine(FACTION, L["CHANGE"])	-- Column headers
 	
 	for key, value in pairs(data) do
-		tooltip:AddDoubleLine(key, value, --[[Left]]0.5, 0.5, 1,	--[[Right]]0.5, 0.5, 1)
+		tooltip:AddDoubleLine(key, value, 0.5, 0.5, 1,	0.5, 0.5, 1)
 	end
 	
 	tooltip:AddLine(" ")
 	-- Money
-	if specialData.Money then
-		tooltip:AddDoubleLine(MONEY, CoppersToString(specialData.Money))
+	if specialData.money then
+		tooltip:AddDoubleLine("|TInterface\\Icons\\INV_Misc_Coin_01:16|t " .. MONEY,
+			CoppersToString(specialData.money))
 	end
 	
-	-- Honor
+	-- Honor & XP
 	local honorGain = GetHonorCurrency() - originalHonor
 	if honorGain ~= 0 then
 		tooltip:AddDoubleLine(HONOR, honorGain)
+	end
+	local xpGain	= UnitXP("player") - originalXP
+	if xpGain ~= 0 then
+		tooltip:AddDoubleLine(COMBAT_XP_GAIN, xpGain)
 	end
 	
     tooltip:Show()
@@ -222,33 +380,52 @@ end
 
 --[[ Menu ]]--
 
-local menuFrame
 local menuList =
 {
 	{	--[[ Consolid8 ]]--
 		text	= addOnName,
-		isTitle = true
+		isTitle = true,
 	},
 	{	-- Reset
 		text 	= RESET,
-		func 	= Consolid8.Reset
+		func 	= Consolid8.Reset,
 	},
 	{	-- Report
 		text	= L["REPORT"],
-		func	= Consolid8.Report
+		func	= Consolid8.Report,
+	},
+	{	--[ ] Loot
+		text	= LOOT,
+		update	= function(tbl)
+			if Consolid8_Settings then tbl.checked = Consolid8_Settings.loot end
+		end,
+		func	= function()
+			Consolid8_Settings.loot = not Consolid8_Settings.loot
+		end,
 	},
 }
 
-function Consolid8.ShowMenu(anchorTo)
-	-- Shows the menu documented in menuList.
-	-- anchorTo: If set, the menu will be anchored to this frame; else, it will be anchored to Consolid8_Frame.
-	-- Create the menu frame if need be
-	if not menuFrame then
-		menuFrame = CreateFrame("Frame", "Consolid8_Menu", Consolid8_Frame, "UIDropDownMenuTemplate")
+function Consolid8.MenuFunction()
+	for key, value in pairs(menuList) do
+		if value.update then value:update()	end
+		UIDropDownMenu_AddButton(value)
 	end
-	
-	-- Set the anchor
-	menuFrame:SetPoint("TOPRIGHT", (anchorTo or Consolid8_Frame), "TOPLEFT")
-	
-	EasyMenu(menuList, menuFrame, "Consolid8_Menu", 0, 0, "MENU")
 end
+
+function Consolid8.ShowMenu()
+	ToggleDropDownMenu(1, nil, Consolid8_Menu, "cursor", 0, 0)
+end
+
+--[[ Broker plugin ]]--
+
+local dataObj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Consolid8",
+	{
+		type 	= "data source",
+		icon 	= [[Interface\AddOns\Consolid8\Broker]],
+		label 	= addOnName,
+		
+		-- Scripts
+		OnClick = Consolid8.ShowMenu,
+		OnEnter	= Consolid8.ShowTooltip,
+		OnLeave = Consolid8.HideTooltip,
+	})
--- locale.lua
+++ locale.lua
@@ -1,39 +1,25 @@
---[[	
-	Consolid8, a World of Warcraft chat frame addon
-	Copyright 2010 Harry Cutts
+--[[
+Consolid8, a World of Warcraft chat frame addon
+Copyright 2010 Harry Cutts
 
-	This file is part of Consolid8.
+This file is part of Consolid8.
 
-	Consolid8 is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+Consolid8 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    Consolid8 is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+Consolid8 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Consolid8.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with Consolid8.  If not, see <http://www.gnu.org/licenses/>.
 ]]--
-
---[[Blizzard Localization used (not included in locale table):
-	RESET
-	FACTION
-	MONEY
-	
-	Blizzard Localization processed (in-file):
-	FACTION_STANDING_DECREASED
-	FACTION_STANDING_INCREASED
-	GOLD_AMOUNT
-	SILVER_AMOUNT
-	COPPER_AMOUNT
-]]
 
 local function CreateFactionPattern(str)
 	-- Returns: a pattern string used to process reputation gain messages.
-	-- 	   str: the format string from which to create the pattern string.
 	local returnStr = str:replace("%s", "(.*)",  0, true)
 	returnStr = returnStr:replace("%d", "(%d*)", 0, true)
 	return returnStr
@@ -41,30 +27,35 @@ end
 
 local function CreateIntegerPattern(str)
 	-- Returns: a pattern string used to process honor and money messages.
-	--     str: the format string from which to create the pattern string.
 	return str:replace("%d", "(%d*)", 0, true)
 end
 
--- Locale
+local function CreateStringPattern(str)
+	-- Returns: a pattern string used to process loot messages.
+	return str:replace("%s", "(.*)", 0, true)
+end
 
-local name, L = ...
+local addOnName, L = ...
 
 --[[ Localised strings ]]--
-L["CHANGE"]			= "Change"
-L["REPORT"]			= "Report"
+L["CHANGE"]	= "Change"
+L["REPORT"]	= "Report"
+L["SHOW"]	= "Show"
+L["HIDE"]	= HIDE
+--[[Show translations 'borrowed' from Omen Threat Meter, credits go to:
+Ben (Aesyl - US Tanaris), Pettigrow, Stan (Arcádia - EU Zirkel des Cenarius), Ananhaid, StingerSoft, and Sayclub.
+Thanks guys!]]
 	
 --[[ Processed Blizzard Localisation ]]--
 -- Pattern strings
-L["REP_DEC"]  		= CreateFactionPattern	(FACTION_STANDING_DECREASED)
-L["REP_INC"]		= CreateFactionPattern	(FACTION_STANDING_INCREASED)
+L["REP_DEC"]  		= CreateFactionPattern(FACTION_STANDING_DECREASED)
+L["REP_INC"]		= CreateFactionPattern(FACTION_STANDING_INCREASED)
 
-L["MONEY_GOLD"]		= CreateIntegerPattern	(GOLD_AMOUNT)
-L["MONEY_SILVER"]	= CreateIntegerPattern	(SILVER_AMOUNT)
-L["MONEY_COPPER"]	= CreateIntegerPattern	(COPPER_AMOUNT)
+L["MONEY_GOLD"]		= CreateIntegerPattern(GOLD_AMOUNT)
+L["MONEY_SILVER"]	= CreateIntegerPattern(SILVER_AMOUNT)
+L["MONEY_COPPER"]	= CreateIntegerPattern(COPPER_AMOUNT)
 
-L["HONOR_1"]		= CreateIntegerPattern	(COMBATLOG_HONORGAIN)
-L["HONOR_2"]		= CreateIntegerPattern	(COMBATLOG_HONORAWARD)
-L["HONOR_3"]		= CreateIntegerPattern	(COMBATLOG_HONORGAIN_NO_RANK)
+L["LOOT"]			= CreateStringPattern (LOOT_ITEM_SELF)
 
 
 local loc = GetLocale()
@@ -72,25 +63,31 @@ local loc = GetLocale()
 if loc == "frFR" then
 	L["CHANGE"] = "Changement"
 	L["REPORT"] = "Rapport"
-	
+	L["SHOW"]	= "Afficher"
+
 elseif loc == "deDE" then
 	L["CHANGE"] = "Änderung"
 	L["REPORT"] = "Report"
-	
+	L["SHOW"]	= "Zeige"
+
 elseif (loc == "esES") or (loc == "esMX") then
 	L["CHANGE"] = "Cambio"
 	L["REPORT"] = "Informe"
-	
+	L["SHOW"]	= "Mostrar"
+
 elseif loc == "ruRU" then
 	L["CHANGE"] = "Изменение"
 	L["REPORT"] = "Рапорт"
-	
+	L["SHOW"]	= "Показать"
+
 elseif loc == "zhCN" then
 	L["CHANGE"] = "变动"
 	L["REPORT"] = "报告"
-	
+	L["SHOW"]	= "显示"
+
 elseif loc == "zhTW" then
 	L["CHANGE"] = "變動"
 	L["REPORT"] = "報告"
-	
+	L["SHOW"]	= "顯示"
+
 end
